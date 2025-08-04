43b77a3cc81afae96828e614cd153365443331c9
fsb <aoszbl@proton.me>
2025-08-04 11:51:05 -0300

    first commit! \o/

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+GNN gym
+
+Exercise with examples how a graph neural network (GNN) works
+"""
+
+def main() -> None:
+    """
+    Main entry point
+    """
+    return
+
+if __name__ == "__main__":
+    main()
