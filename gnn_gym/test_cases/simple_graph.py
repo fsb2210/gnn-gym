44d4e4d7d361bd949c9102b940d72fbbafb153ab
fsb <aoszbl@proton.me>
2025-08-05 15:25:27 -0300

    add two functions: get_config & create_dataset
    get_config: establish baseline model
    create_dataset: create dictionary with keys corresponding to a graph as
    is typical of pytorch_geometric: x, y and edge_index

--- gnn_gym/test_cases/simple_graph.py
+++ gnn_gym/test_cases/simple_graph.py
@@ -2,24 +2,31 @@
 Simple graph neural network
 """
 
-from typing import Dict
+from typing import Any, Dict
+from ..nn import GCNLayer
+
+import torch
+import torch.nn as nn
 
 def get_config() -> Dict:
-
-    config = {
-        "name": "Simple Graph",
-        "description": "4-node simple graph",
+    return {
+        "name": "Simple Graph neural network on an undirected graph",
+        "description": "Example of a GCN neural network",
+        "model": "GCN",
         "task": "node_classification",
         "num_nodes": 4,
         "num_edges": 8,
-        "feature_dim": 3,
-        "model": "GCN",
-        "epochs": 4,
+        "features_dim": 3,
+        "activation": "relu",
+        "epochs": 2,
     }
-    return config
 
-def run(config: Dict):
-    raise NotImplementedError("run method not implemented yet")
+def create_dataset(config: Dict) -> Dict:
+    N: int = config.get("num_nodes", -1)
+    n_feats: int = config.get("features_dim", -1)
+    # check for >= 0 values
+    if N <= 0 or n_feats <= 0:
+        raise ValueError(f"Either N={N} or n_feats={n_feats} is <= 0!")
 
 class SimpleGNN():
     def __init__(self) -> None:
